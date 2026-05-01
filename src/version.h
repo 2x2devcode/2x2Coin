cab908ebbede59d49810453c5de263fa2fc87379
coindev <webmastercase20@gmail.com>
2026-05-01 19:36:42 -0300

    Update
    Increment protocol and version constants

--- src/version.h
+++ src/version.h
@@ -9,7 +9,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70025;
+static const int PROTOCOL_VERSION = 70026;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -35,13 +35,13 @@ static const int BIP0031_VERSION = 60000;
 static const int MEMPOOL_GD_VERSION = 60002;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 70023;
+static const int NO_BLOOM_VERSION = 70024;
 
 //! "sendheaders" command and announcing blocks with headers starts with this version
-static const int SENDHEADERS_VERSION = 70024;
+static const int SENDHEADERS_VERSION = 70025;
 
 //! "feefilter" tells peers to filter invs to you by fee starts with this version
-static const int FEEFILTER_VERSION = 70025;
+static const int FEEFILTER_VERSION = 70026;
 
 /*
 // Disable BIP152
